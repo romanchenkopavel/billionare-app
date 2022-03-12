b169f3f091359a6b4a8696e9b1c8448551c60423
romanchenkopavel <pavlo.romanchenko@auto1.com>
2022-03-12 13:57:29 +0200

    chore: setup prettier, pre-commit hooks and eslint

---
+++ lint-staged.js
@@ -0,0 +1,9 @@
+const config = {
+    '*.{js,jsx,ts,tsx}': [
+        'eslint --max-warnings=0',
+        'react-scripts test --bail --watchAll=false --findRelatedTests --passWithNoTests',
+        () => 'tsc-files --noEmit',
+    ],
+};
+
+export default config;
