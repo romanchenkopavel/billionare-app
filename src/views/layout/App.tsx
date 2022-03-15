d9850b73f23ccb87db921186b97f6b53fbaa4d5b
romanchenkopavel <pavlo.romanchenko@auto1.com>
2022-03-16 00:03:54 +0200

    feat: wrap root in game flow state and dispatch providers

--- src/views/layout/App.tsx
+++ src/views/layout/App.tsx
@@ -1,7 +1,30 @@
-import { WelcomeScreen } from 'views/welcome-screen/components';
+import { useCallback, useMemo, useState } from 'react';
+
+import WelcomeScreen from 'views/welcome-screen/components';
+
+import { GameFlowContext, GameFlowDispatchContext } from 'shared/context';
+import { GameFlowParams, GameSteps } from 'shared/types';
 
 function App() {
-    return <WelcomeScreen />;
+  const [gameState, setGameStep] = useState<GameFlowParams>({
+    step: GameSteps.Start,
+  });
+
+  const value = useMemo(() => gameState, [gameState]);
+  const dispatchGameStep = useCallback(
+    (step: keyof typeof GameSteps) => {
+      setGameStep({ step });
+    },
+    [setGameStep],
+  );
+
+  return (
+    <GameFlowContext.Provider value={value}>
+      <GameFlowDispatchContext.Provider value={dispatchGameStep}>
+        <WelcomeScreen />
+      </GameFlowDispatchContext.Provider>
+    </GameFlowContext.Provider>
+  );
 }
 
 export default App;
--- src/views/layout/__tests__/App.test.tsx
+++ src/views/layout/__tests__/App.test.tsx
@@ -3,10 +3,10 @@ import { render, screen } from '@testing-library/react';
 import App from '../App';
 
 describe('App', () => {
-    test('rendering component', () => {
-        render(<App />);
-        const linkElement = screen.getByText('Who wants to be a millionaire?');
+  test('rendering component', () => {
+    render(<App />);
+    const linkElement = screen.getByText('Who wants to be a millionaire?');
 
-        expect(linkElement).toBeInTheDocument();
-    });
+    expect(linkElement).toBeInTheDocument();
+  });
 });
