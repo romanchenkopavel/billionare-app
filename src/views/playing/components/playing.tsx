04d39b75c6463b71635403b8b55061e86451cb1f
romanchenkopavel <pavlo.romanchenko@auto1.com>
2022-03-16 22:12:17 +0200

    feat: add playing component

--- src/views/playing/components/playing.tsx
+++ src/views/playing/components/playing.tsx
@@ -1,5 +1,20 @@
+import Options from './options';
+
+import styles from './playing.module.css';
+import Rewards from './rewards';
+
 function Playing() {
-  return <div>Game Process</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.qaSection}>
+        <span className={styles.question}>
+          How old your elder brother was 10 years before you was born, mate?
+        </span>
+        <Options />
+      </div>
+      <Rewards />
+    </div>
+  );
 }
 
 export default Playing;
